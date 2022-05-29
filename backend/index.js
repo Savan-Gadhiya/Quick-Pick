ce2a31eb5fb18037863654e14c95206992f3a64f
Savan-Gadhiya <gadhiyasavan001@gmail.com>
2022-05-29 22:47:57 +0530

    for deployment

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,10 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.json({message: "Server Working..."});
+})
 app.use('/api', routes);
 
 
